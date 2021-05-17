02928d430a9f5d8abd24ddd1be24e8a37555f9b3
akotov777 <akotov777@gmail.com>
2021-05-17 23:50:10 +0500

    Reworked player movement to PlayerFeature
    Added PlayerMovementSettings scriptable object
    Changerd NetworkServices to initialize it _pool field in Start()
    Added property in CustomNetworlManager for PrefabsToSpawn
    Added initialization for _prefabs field for SpawnerPool in Start() using reference from CustomNetworkManager

---
+++ Assets/Scripts/Data/PlayerMovementSettings.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+
+[CreateAssetMenu(fileName = "PlayerMovementSettings", menuName = "ScriptableObjects/PlayerMovementSettings")]
+public sealed class PlayerMovementSettings : ScriptableObject
+{
+    public float Speed = 6.0f;
+    public float JumpSpeed = 8.0f;
+    public float Gravity = 20.0f;
+
+    public float XSensitivity = 2f;
+    public float YSensitivity = 2f;
+    public float MinimumX = -90F;
+    public float MaximumX = 90F;
+}
---
+++ Assets/Scripts/Model/IPlayerFeature/MovementFeature.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using UnityEngine;
+
+
+public class MovementFeature : IPlayerFeature
+{
+    #region Fields
+
+    private CharacterController _characterController;
+    private Camera _camera;
+    private PlayerMovementSettings _settings;
+    private Vector3 _moveDirection = Vector3.zero;
+    private Quaternion _characterTargetRotate;
+    private Quaternion _cameraTargetRotate;
+
+    #endregion
+
+
+    #region Properties
+
+
+
+    #endregion
+
+
+    #region ClassLifeCycles
+
+    public MovementFeature(Camera camera, CharacterController characterController)
+    {
+        _camera = camera;
+        _characterController = characterController;
+        _settings = Resources.Load<PlayerMovementSettings>("Data/PlayerMovementSettings");
+
+        _characterTargetRotate = _camera.transform.localRotation;
+        _cameraTargetRotate = _camera.transform.localRotation;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    #endregion
+
+
+    #region Methods
+
+    private void LookRotation(Transform character, Transform camera)
+    {
+        float yRot = Input.GetAxis("Mouse X") * _settings.XSensitivity;
+        float xRot = Input.GetAxis("Mouse Y") * _settings.YSensitivity;
+
+        _characterTargetRotate *= Quaternion.Euler(0f, yRot, 0f);
+        _cameraTargetRotate *= Quaternion.Euler(-xRot, 0f, 0f);
+
+        _cameraTargetRotate = ClampRotationAroundXAxis(_cameraTargetRotate);
+
+        character.localRotation = _characterTargetRotate;
+        camera.localRotation = _cameraTargetRotate;
+    }
+
+    private Quaternion ClampRotationAroundXAxis(Quaternion q)
+    {
+        q.x /= q.w;
+        q.y /= q.w;
+        q.z /= q.w;
+        q.w = 1.0f;
+
+        float angleX = 2.0f * Mathf.Rad2Deg * Mathf.Atan(q.x);
+
+        angleX = Mathf.Clamp(angleX, _settings.MinimumX, _settings.MaximumX);
+
+        q.x = Mathf.Tan(0.5f * Mathf.Deg2Rad * angleX);
+
+        return q;
+    }
+
+    #endregion
+
+
+    #region IPlayerFeature
+
+    public void ExecuteFeature()
+    {
+        if (_characterController.isGrounded)
+        {
+            Vector3 desiredMove = _characterController.transform.forward * Input.GetAxis("Vertical") + _characterController.transform.right * Input.GetAxis("Horizontal");
+            _moveDirection = desiredMove * _settings.Speed;
+
+            if (Input.GetButton("Jump"))
+            {
+                _moveDirection.y = _settings.JumpSpeed;
+            }
+        }
+
+        _moveDirection.y -= _settings.Gravity * Time.deltaTime;
+
+        _characterController.Move(_moveDirection * Time.deltaTime);
+
+        LookRotation(_characterController.transform, _camera.transform);
+    }
+
+    #endregion
+}
