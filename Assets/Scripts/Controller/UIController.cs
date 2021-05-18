c3ea140c2900a6ec25fa229ef35d94fb3f28b289
akotov777 <akotov777@gmail.com>
2021-05-18 21:04:23 +0500

    Added UIController

---
+++ Assets/Scripts/Controller/UIController.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+
+
+public class UIController : MonoBehaviour
+{
+	#region Fields
+
+	[SerializeField] private BaseUI _mainMenu;
+	[SerializeField] private BaseUI _gameMenu;
+
+	#endregion
+
+
+	#region Properties
+
+
+
+	#endregion
+
+
+	#region UnityMethods
+
+	void Start()
+	{
+		CloseGameMenu();
+	}
+
+	void Update()
+	{
+
+	}
+
+	#endregion
+
+
+	#region Methods
+
+	public void ShowMainMenu()
+	{
+		_mainMenu.ToggleOn();
+	}
+
+	public void ShowGameMenu()
+	{
+		_gameMenu.ToggleOn();
+	}
+
+	public void CloseMainMenu()
+    {
+		_mainMenu.ToggleOff();
+    }
+
+	public void CloseGameMenu()
+    {
+		_gameMenu.ToggleOff();
+    }
+
+
+	#endregion
+}
