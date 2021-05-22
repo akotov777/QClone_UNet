d27040af73c7853f9ec5e7c478e45a5e504279d6
akotov777 <akotov777@gmail.com>
2021-05-22 19:18:40 +0500

    Added LookingFeature changed MovementFeature

---
+++ Assets/Scripts/Model/IPlayerFeature/LookingFeature.cs
@@ -0,0 +1,86 @@
+﻿using UnityEngine;
+
+
+public class LookingFeature : BasePlayerFeature
+{
+    #region Fields
+
+    private CharacterController _characterController;
+    private Camera _camera;
+    private PlayerMovementSettings _settings;
+    private Quaternion _characterTargetRotate;
+    private Quaternion _cameraTargetRotate;
+
+    #endregion
+
+
+    #region Properties
+
+
+
+    #endregion
+
+
+    #region ClassLifeCycles
+
+    public LookingFeature(Camera camera, CharacterController characterController)
+    {
+        _camera = camera;
+        _characterController = characterController;
+        _settings = Resources.Load<PlayerMovementSettings>("Data/PlayerMovementSettings");
+
+        _characterTargetRotate = _camera.transform.localRotation;
+        _cameraTargetRotate = _camera.transform.localRotation;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    #endregion
+
+
+    #region Methods
+
+    private void LookRotation(Transform camera, Transform cameraRotationFollower)
+    {
+        float yRot = Input.GetAxis("Mouse X") * _settings.XSensitivity;
+        float xRot = Input.GetAxis("Mouse Y") * _settings.YSensitivity;
+
+        _characterTargetRotate *= Quaternion.Euler(0f, yRot, 0f);
+        _cameraTargetRotate *= Quaternion.Euler(-xRot, 0f, 0f);
+
+        _cameraTargetRotate = ClampRotationAroundXAxis(_cameraTargetRotate);
+
+        cameraRotationFollower.localRotation = _characterTargetRotate;
+        camera.localRotation = _cameraTargetRotate;
+    }
+
+    private Quaternion ClampRotationAroundXAxis(Quaternion q)
+    {
+        q.x /= q.w;
+        q.y /= q.w;
+        q.z /= q.w;
+        q.w = 1.0f;
+
+        float angleX = 2.0f * Mathf.Rad2Deg * Mathf.Atan(q.x);
+
+        angleX = Mathf.Clamp(angleX, _settings.MinimumX, _settings.MaximumX);
+
+        q.x = Mathf.Tan(0.5f * Mathf.Deg2Rad * angleX);
+
+        return q;
+    }
+
+    #endregion
+
+
+    #region IPlayerFeature
+
+    public override void ExecuteFeature()
+    {
+        if (!IsActive)
+            return;
+
+        LookRotation(_characterController.transform, _camera.transform);
+    }
+
+    #endregion
+}
--- Assets/Scripts/Model/IPlayerFeature/MovementFeature.cs
+++ Assets/Scripts/Model/IPlayerFeature/MovementFeature.cs
@@ -6,11 +6,8 @@ public class MovementFeature : BasePlayerFeature
     #region Fields
 
     private CharacterController _characterController;
-    private Camera _camera;
     private PlayerMovementSettings _settings;
     private Vector3 _moveDirection = Vector3.zero;
-    private Quaternion _characterTargetRotate;
-    private Quaternion _cameraTargetRotate;
 
     #endregion
 
@@ -24,15 +21,10 @@ public class MovementFeature : BasePlayerFeature
 
     #region ClassLifeCycles
 
-    public MovementFeature(Camera camera, CharacterController characterController)
+    public MovementFeature(CharacterController characterController)
     {
-        _camera = camera;
         _characterController = characterController;
         _settings = Resources.Load<PlayerMovementSettings>("Data/PlayerMovementSettings");
-
-        _characterTargetRotate = _camera.transform.localRotation;
-        _cameraTargetRotate = _camera.transform.localRotation;
-        Cursor.lockState = CursorLockMode.Locked;
     }
 
     #endregion
@@ -40,35 +32,7 @@ public class MovementFeature : BasePlayerFeature
 
     #region Methods
 
-    private void LookRotation(Transform character, Transform camera)
-    {
-        float yRot = Input.GetAxis("Mouse X") * _settings.XSensitivity;
-        float xRot = Input.GetAxis("Mouse Y") * _settings.YSensitivity;
 
-        _characterTargetRotate *= Quaternion.Euler(0f, yRot, 0f);
-        _cameraTargetRotate *= Quaternion.Euler(-xRot, 0f, 0f);
-
-        _cameraTargetRotate = ClampRotationAroundXAxis(_cameraTargetRotate);
-
-        character.localRotation = _characterTargetRotate;
-        camera.localRotation = _cameraTargetRotate;
-    }
-
-    private Quaternion ClampRotationAroundXAxis(Quaternion q)
-    {
-        q.x /= q.w;
-        q.y /= q.w;
-        q.z /= q.w;
-        q.w = 1.0f;
-
-        float angleX = 2.0f * Mathf.Rad2Deg * Mathf.Atan(q.x);
-
-        angleX = Mathf.Clamp(angleX, _settings.MinimumX, _settings.MaximumX);
-
-        q.x = Mathf.Tan(0.5f * Mathf.Deg2Rad * angleX);
-
-        return q;
-    }
 
     #endregion
 
@@ -96,8 +60,6 @@ public class MovementFeature : BasePlayerFeature
         _moveDirection.y -= _settings.Gravity * Time.deltaTime;
 
         _characterController.Move(_moveDirection * Time.deltaTime);
-
-        LookRotation(_characterController.transform, _camera.transform);
     }
 
     #endregion
