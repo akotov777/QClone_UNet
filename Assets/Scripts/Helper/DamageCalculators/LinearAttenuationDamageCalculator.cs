187f35c234e42e615f9849faea7eeac6c49b57f9
akotov777 <akotov777@gmail.com>
2021-05-27 16:41:56 +0500

    Added base class for damage calculations

---
+++ Assets/Scripts/Helper/DamageCalculators/LinearAttenuationDamageCalculator.cs
@@ -0,0 +1,11 @@
+﻿public class LinearAttenuationDamageCalculator : DamageCalculator
+{
+    #region Methods
+
+    public override int CalculateDamage(float incomingDamage, float attenuationFactor)
+    {
+        return (int)(incomingDamage / attenuationFactor);
+    }
+
+    #endregion
+}
