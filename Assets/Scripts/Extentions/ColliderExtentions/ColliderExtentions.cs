e7e9f7b2e81df43cb4a2a2fff9cb9029aea206bd
akotov777 <akotov777@gmail.com>
2021-05-27 15:59:54 +0500

    Added custom collision handler extention system for Unity Collider class

---
+++ Assets/Scripts/Extentions/ColliderExtentions/ColliderExtentions.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+
+public static class ColliderExtentions
+{
+    #region Fields
+
+    private static Dictionary<Collider, List<ICollisionHandler>> _handlersTable;
+
+    #endregion
+
+
+    #region Methods
+
+    public static void AddCollisionHandler(this Collider collider, ICollisionHandler collisionHandler)
+    {
+        if (_handlersTable.ContainsKey(collider))
+        {
+            _handlersTable[collider].Add(collisionHandler);
+        }
+        else
+        {
+            var newList = new List<ICollisionHandler>();
+            newList.Add(collisionHandler);
+            _handlersTable.Add(collider, newList);
+        }
+    }
+
+    public static void HandleCollision(this Collider collider)
+    {
+        if (!_handlersTable.ContainsKey(collider))
+        {
+            return;
+        }
+        foreach (var handler in _handlersTable[collider])
+        {
+            handler.HandleCollision();
+        }
+    }
+
+    #endregion
+}
