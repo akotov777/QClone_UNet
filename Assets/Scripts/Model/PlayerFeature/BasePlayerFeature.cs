77e69967354a7d78fa97b34cf62eb1da76287e21
akotov777 <akotov777@gmail.com>
2021-05-28 19:28:01 +0500

    Added OnEnable OnDisable actions to BasePlayerFeature

--- Assets/Scripts/Model/PlayerFeature/BasePlayerFeature.cs
+++ Assets/Scripts/Model/PlayerFeature/BasePlayerFeature.cs
@@ -1,8 +1,13 @@
-﻿public abstract class BasePlayerFeature
+﻿using System;
+
+
+public abstract class BasePlayerFeature
 {
     #region Fields
 
     internal bool _isActive = true;
+    internal Action OnEnable;
+    internal Action OnDisable;
 
     #endregion
 
@@ -12,7 +17,18 @@
     public bool IsActive
     {
         get { return _isActive; }
-        set { _isActive = value; }
+        set
+        {
+            bool oldValue = _isActive;
+            _isActive = value;
+            if (oldValue != _isActive)
+            {
+                if (oldValue == true)
+                    OnDisable.Invoke();
+                else
+                    OnEnable.Invoke();
+            }
+        }
     }
 
     #endregion
---
+++ Assets/Scripts/Model/PlayerFeature/RespawnFeature.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using UnityEngine;
+
+
+public class RespawnFeature : BasePlayerFeature
+{
+    #region Fields
+
+
+
+    #endregion
+
+
+    #region ClassLifeCycles
+
+
+
+    #endregion
+
+
+    #region Properties
+
+
+
+    #endregion
+
+
+    #region Methods
+
+
+
+    #endregion
+}
