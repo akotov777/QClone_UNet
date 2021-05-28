22cddab25de5cab4386428581dbec2d6db23499a
akotov777 <akotov777@gmail.com>
2021-05-28 15:04:40 +0500

    Added Constants static class

---
+++ Assets/Scripts/Data/Constants.cs
@@ -0,0 +1,10 @@
+﻿public static class Constants
+{
+    public static class ResourcesPaths
+    {
+        public static class Materials
+        {
+            public const string Default = "Materials/Default";
+        }
+    }
+}
