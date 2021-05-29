e624dc50d32dcf7385c6f87dc44ab780c7296715
akotov777 <akotov777@gmail.com>
2021-05-29 14:32:25 +0500

    Added RepeatableAction system

---
+++ Assets/Scripts/RepeatableAction/IRepeatableAction.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+
+public interface IRepeatableAction
+{
+    #region Properties
+
+    Action Method { get; }
+    bool IsRepeating { get; }
+    float TimeToInvoke { get; }
+    float RemainingTime { get; set; }
+
+    #endregion
+}
---
+++ Assets/Scripts/RepeatableAction/RepeatableActionController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public sealed class RepeatableActionController
+{
+    #region Fields
+
+    private readonly List<IRepeatableAction> _timeRemainings;
+
+    #endregion
+
+
+    #region ClassLifeCycles
+
+    public RepeatableActionController()
+    {
+        _timeRemainings = RepeatableActionExtentions.TimeRemainings;
+    }
+
+    #endregion
+
+
+    #region IExecute
+
+    public void Execute()
+    {
+        var time = Time.deltaTime;
+        for (var i = 0; i < _timeRemainings.Count; i++)
+        {
+            var obj = _timeRemainings[i];
+            obj.RemainingTime -= time;
+            if (obj.RemainingTime <= 0.0f)
+            {
+                obj.Method.Invoke();
+                if (!obj.IsRepeating)
+                    obj.RemoveTimeRemaining();
+                else
+                    obj.RemainingTime = obj.TimeToInvoke;
+            }
+        }
+    }
+
+    #endregion
+}
